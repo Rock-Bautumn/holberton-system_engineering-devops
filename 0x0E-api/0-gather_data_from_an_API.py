803b20b75edf8e4870165a6175da52155a4f6a5f
Rock-Bautumn <3623@holbertonschool.com>
2022-03-24 18:28:39 -0500

    Initial commit for 0x0E api, adding task 0

---
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Pulls some 'employee' data from a fake api
+"""
+
+
+if __name__ == "__main__":
+    usersurl = "https://jsonplaceholder.typicode.com/users/"
+    todosurl = "https://jsonplaceholder.typicode.com/todos/?userId="
+    from sys import argv
+    try:
+        employeeid = int(argv[1])
+    except (ValueError, TypeError):
+        raise TypeError(
+            "Employee ID must be an integer. Syntax: {} <employeeid>".format(
+                argv[0]))
+    # print(employeeid)
+    requesturl = usersurl + str(employeeid)
+    import requests
+    response = requests.get(requesturl)
+    # print(response.json())
+    reply = response.json()
+    # print(reply["name"])
+    name = reply["name"]
+    requesturl = todosurl + str(employeeid)
+    response = requests.get(requesturl)
+    reply = response.json()
+    # print(reply)
+    # print(len(reply))
+    tasksqty = len(reply)
+    requesturl = requesturl + "&completed=true"
+    response = requests.get(requesturl)
+    reply = response.json()
+    completedtasksqty = len(reply)
+    # print(len(reply))
+    print("Employee {} is done with tasks({}/{}):".format(
+        name, completedtasksqty, tasksqty
+    ))
+    for task in reply:
+        # print("task title: {}".format(task["title"]))
+        print("\t {}".format(task["title"]))
---
+++ 0x0E-api/README.md
@@ -0,0 +1,5 @@
+**0x0E-api**
+
+*Did you GET what they're serving?*
+
+In this project we learn RESTful APIs
