5aa739dc3f01d89a9501d7abfb0d02eb0091c491
Rock-Bautumn <3623@holbertonschool.com>
2022-03-24 21:58:04 -0500

    Adding task 1 in 0x0E api

---
+++ 0x0E-api/1-export_to_CSV.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Pulls some 'employee' data from a fake api
+"""
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """the main code"""
+    usersurl = "https://jsonplaceholder.typicode.com/users/"
+    todosurl = "https://jsonplaceholder.typicode.com/todos/?userId="
+    try:
+        employeeid = int(argv[1])
+    except (ValueError, TypeError):
+        raise TypeError(
+            "Employee ID must be an integer. Syntax: {} <employeeid>".format(
+                argv[0]))
+    # print(employeeid)
+    requesturl = usersurl + str(employeeid)
+    response = requests.get(requesturl)
+    # print(response.json())
+    reply = response.json()
+    # print(reply["name"])
+    name = reply.get("name")
+    username = reply.get("username")
+    requesturl = todosurl + str(employeeid)
+    response = requests.get(requesturl)
+    reply = response.json()
+    with open('names.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile, dialect=csv.unix_dialect)
+        for item in reply:
+            # Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+            csvwriter.writerow(
+                [employeeid, username, item.get("completed"), item.get("title")])
+    # print(reply)
+    # print(len(reply))
+    tasksqty = len(reply)
+    requesturl = requesturl + "&completed=true"
+    response = requests.get(requesturl)
+    reply = response.json()
+    completedtasksqty = len(reply)
+    # print(len(reply))
+    print("Employee {} is done with tasks({}/{}):".format(
+        name, completedtasksqty, tasksqty
+    ))
+    for task in reply:
+        # print("task title: {}".format(task["title"]))
+        print("\t {}".format(task.get("title")))
---
+++ 0x0E-api/test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""testing file"""
+
+
+import csv
+
+with open('names.csv', 'w') as csvfile:
+    fieldnames = ['first_name', 'last_name']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, dialect=csv.unix_dialect)
+
+    writer.writeheader()
+    # writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
+    # writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
+    # writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
+    writer.writerow({'Baked', 'Beans'})
+    writer.writerow({'Lovely', 'Spam'})
+    writer.writerow({'Wonderful', 'Spam'})
